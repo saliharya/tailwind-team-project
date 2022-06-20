e0301e20ad4d22e9b43ee2b1d9c4ff3be4095039
Salih Arya Gumilang <saliharyazinc@gmail.com>
2022-06-20 23:43:35 +0700

    set container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        'primary': '#0070f3',
+        'secondary': '#f5f5f5',
+      },
+      screens:{
+        '2xl': '1320px',
+      }
+    },
   },
   plugins: [],
 }
